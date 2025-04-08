d9381f8a2bba71cb00275499d7df52c458f0f47d
Yago Garcia <yagog5002@gmail.com>
2025-04-08 18:57:24 +0200

    Entity Categoria + schema

---
+++ llibresApp/src/main/java/com/iticbcn/mywebapp/llibresApp/DomainModel/Categoria.java
@@ -0,0 +1,34 @@
+package com.iticbcn.mywebapp.llibresApp.DomainModel;
+
+import java.security.Timestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name="category")
+public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id_Categoria")
+    private Long id_Categoria ;
+    @Column(nullable = false)
+    private String desc_Categoria;
+    @Column(nullable = false)
+    private String status_Categoria;
+    @Column(nullable = false)
+    private Timestamp creation_at;
+    @Column(nullable = false)
+    private Timestamp updated_at ;
+    // @Column(nullable = false)
+    // private Subcategoria subcategoria;
+}
+
--- llibresApp/src/main/resources/schema.sql
+++ llibresApp/src/main/resources/schema.sql
@@ -7,3 +7,11 @@ CREATE TABLE IF NOT EXISTS books (
     tematica VARCHAR(150),
     isbn VARCHAR (13) UNIQUE NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS category (
+    id_Categoria BIGINT AUTO_INCREMENT PRIMARY KEY,
+    desc_Categoria VARCHAR(255) NOT NULL,
+    status_Categoria VARCHAR(50) NOT NULL,
+    creation_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+);
