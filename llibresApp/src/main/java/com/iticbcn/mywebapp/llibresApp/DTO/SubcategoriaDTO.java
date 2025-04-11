1419d29efc614e92672d19906fe3e817e26d35f2
Karen <2023_karen.medrano@iticbcn.cat>
2025-04-11 18:53:52 +0200

    p

--- llibresApp/src/main/java/com/iticbcn/mywebapp/llibresApp/DTO/SubcategoriaDTO.java
+++ llibresApp/src/main/java/com/iticbcn/mywebapp/llibresApp/DTO/SubcategoriaDTO.java
@@ -1,5 +1,24 @@
 package com.iticbcn.mywebapp.llibresApp.DTO;
 
+import java.security.Timestamp;
+
+import com.iticbcn.mywebapp.llibresApp.DomainModel.Categoria;
+import com.iticbcn.mywebapp.llibresApp.DomainModel.Product;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class SubcategoriaDTO {
+
+    private String desc_Subcategoria;
+    
+    private String status_Subcategoria;
+
+    private Categoria categoria;
+
+    private Product product;
     
 }
