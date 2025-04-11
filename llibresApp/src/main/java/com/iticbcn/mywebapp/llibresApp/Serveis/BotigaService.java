e8abbad9a252d8a419a6fb352426f5b5e959cf6f
Karen <2023_karen.medrano@iticbcn.cat>
2025-04-11 12:04:50 +0200

    BotigaService

--- llibresApp/src/main/java/com/iticbcn/mywebapp/llibresApp/Serveis/BotigaService.java
+++ llibresApp/src/main/java/com/iticbcn/mywebapp/llibresApp/Serveis/BotigaService.java
@@ -6,10 +6,16 @@ import java.util.Set;
 import com.iticbcn.mywebapp.llibresApp.DTO.LlibreDTO;
 import com.iticbcn.mywebapp.llibresApp.DomainModel.Product;
 
-public interface ProductService {
- 
+public interface BotigaService {
+  
     Set<Product> findAllProducts();
     Product findProductsByName(String name);
     Set<Product> findAllProducts(String subcategory);
     void increasePrice(Product product);
+
+/*  List<T> findAll();
+    Optional<T> findById(ID id);
+    T save(T entity);
+    void deleteById(ID id); */
+   
 }
