5958be73225810b352145b0b446d12fea102fa41
Yago Garcia <yagog5002@gmail.com>
2025-04-21 18:32:20 +0200

    schema

--- llibresApp/src/main/resources/schema.sql
+++ llibresApp/src/main/resources/schema.sql
@@ -1,35 +1,39 @@
+CREATE DATABASE IF NOT EXISTS botiga;
+
+USE botiga;
+
 CREATE TABLE IF NOT EXISTS `categories` (
   `id_categoria` bigint(20) NOT NULL AUTO_INCREMENT,
-  `desc_categoria` varchar(255) DEFAULT NULL,
-  `status_categoria` varchar(50) DEFAULT NULL,
-  `creation_at` datetime(6) DEFAULT NULL,
-  `updated_at` datetime(6) DEFAULT NULL,
+  `desc_categoria` varchar(255) NOT NULL,
+  `status_categoria` varchar(50) NOT NULL,
+  `creation_at` datetime(6) NOT NULL,
+  `updated_at` datetime(6) NOT NULL,
   PRIMARY KEY (`id_categoria`)
 );
 
 CREATE TABLE IF NOT EXISTS `subcategories` (
   `id_subcategoria` bigint(20) NOT NULL AUTO_INCREMENT,
-  `desc_subcategoria` varchar(255) DEFAULT NULL,
-  `status_subcategoria` varchar(50) DEFAULT NULL,
-  `categoria_id` bigint(20) NOT NULL,
-  `creation_at` datetime(6) DEFAULT NULL,
-  `updated_at` datetime(6) DEFAULT NULL,
+  `desc_subcategoria` varchar(255) NOT NULL,
+  `status_subcategoria` varchar(50) NOT NULL,
+  `categoria_id` bigint(20) NOT NULL, 
+  `creation_at` datetime(6) NOT NULL,
+  `updated_at` datetime(6) NOT NULL,
   PRIMARY KEY (`id_subcategoria`),
-  CONSTRAINT `fk_subcategoria_categoria` FOREIGN KEY (`categoria_id`) REFERENCES `categories` (`id_categoria`) ON DELETE CASCADE
+  CONSTRAINT `fk_subcategoria_categoria` FOREIGN KEY (`categoria_id`) REFERENCES `categories` (`id_categoria`) ON DELETE CASCADE -- ON DELETE CASCADE si quieres borrar subcategorías al borrar la categoría
 );
 
 CREATE TABLE IF NOT EXISTS `products` (
   `product_id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `company` varchar(255) DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `price` float DEFAULT NULL,
-  `units` bigint(20) DEFAULT NULL,
-  `categoria_id` bigint(20) NOT NULL,
-  `subcategoria_id` bigint(20) NOT NULL,
-  `creation_at` datetime(6) DEFAULT NULL,
-  `updated_at` datetime(6) DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `company` varchar(255) NOT NULL,
+  `price` float NOT NULL,
+  `units` bigint(20) NOT NULL,
+  `categoria_id` bigint(20) NOT NULL, 
+  `subcategoria_id` bigint(20) NOT NULL, 
+  `creation_at` datetime(6) NOT NULL,
+  `updated_at` datetime(6) NOT NULL,
   PRIMARY KEY (`product_id`),
-  CONSTRAINT `fk_product_categoria` FOREIGN KEY (`categoria_id`) REFERENCES `categories` (`id_categoria`) ON DELETE CASCADE,
-  CONSTRAINT `fk_product_subcategoria` FOREIGN KEY (`subcategoria_id`) REFERENCES `subcategories` (`id_subcategoria`) ON DELETE CASCADE
+  CONSTRAINT `fk_product_categoria` FOREIGN KEY (`categoria_id`) REFERENCES `categories` (`id_categoria`) ON DELETE CASCADE, -- ON DELETE CASCADE si quieres borrar productos al borrar la categoría
+  CONSTRAINT `fk_product_subcategoria` FOREIGN KEY (`subcategoria_id`) REFERENCES `subcategories` (`id_subcategoria`) ON DELETE CASCADE -- ON DELETE CASCADE si quieres borrar productos al borrar la subcategoría
 );
