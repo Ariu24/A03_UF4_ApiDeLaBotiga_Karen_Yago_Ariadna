4be84ab9aeff5873de39f9c9239391e1cb65d79b
Karen <2023_karen.medrano@iticbcn.cat>
2025-04-23 11:19:39 +0200

    e

--- llibresApp/src/main/java/com/iticbcn/mywebapp/llibresApp/Mapper/SubcategoriaMapper.java
+++ llibresApp/src/main/java/com/iticbcn/mywebapp/llibresApp/Mapper/SubcategoriaMapper.java
@@ -3,7 +3,6 @@ package com.iticbcn.mywebapp.llibresApp.Mapper;
 import java.util.List;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 import com.iticbcn.mywebapp.llibresApp.DTO.SubcategoriaDTO;
@@ -11,9 +10,8 @@ import com.iticbcn.mywebapp.llibresApp.DomainModel.Subcategoria;
 
 @Mapper(componentModel="spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface SubcategoriaMapper {
-    @Mapping(source = "categoria.descCategoria", target = "descCategoria")
     
-    SubcategoriaDTO SubcategoriaToSubcategoriaDTO(Subcategoria subcategoria);
+SubcategoriaDTO SubcategoriaToSubcategoriaDTO(Subcategoria subcategoria);
     Subcategoria SubcategoriaDTOtoSubcategoria(SubcategoriaDTO SubcategoriaDTO);
 
     List<Subcategoria> SubcategoriaDTOtoSubcategorias(List<SubcategoriaDTO> llibresDTO);
